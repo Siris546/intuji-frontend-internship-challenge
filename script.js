651ca782fba72cb1a88e3e29a143640dd434824b
Siris <seeres60@gmail.com>
2024-01-03 07:28:55 +0545

    working on flip icon

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ var dropdownBtn = document.querySelectorAll('.dropdown-elem');
 //Add this for toggling dropdown
 lastOpened = null;
 const product= document.querySelector('.dropdown-elem');
-console.log(product);
-var menuContent = product.nextElementSibling;
-console.log(menuContent);
+
+const flip  = document.querySelector('.flip');
+
 dropdownBtn.forEach(btn => btn.addEventListener('click', function() {
   var menuContent = this.nextElementSibling;
   if (!menuContent.classList.contains("show")) {
+    flip.classList.add('icon-flipped');
   menuContent.classList.add("show");
   menuContent.classList.remove("hide");
   } else {
   menuContent.classList.add("hide");
   menuContent.classList.remove("show");
+  flip.classList.remove('icon-flipped');
   }
   
   //Add this for toggling dropdown
@@ -20,3 +22,10 @@ dropdownBtn.forEach(btn => btn.addEventListener('click', function() {
       lastOpened.classList.remove("show");
       lastOpened = menuContent;
 }));
+
+const swingsvg = document.querySelector('.swingsvg');
+
+//write a script that will make the svg blink in time interval
+setInterval(function() {
+  swingsvg.classList.toggle('blink');
+}, 100);
