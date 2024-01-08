329f2c88d695a7cff409f2baf6ebeb33ed60f742
Siris <seeres60@gmail.com>
2024-01-08 09:55:37 +0545

    hamburger menu added

--- script.js
+++ script.js
@@ -3,34 +3,48 @@ const faqquestion= document.querySelectorAll('.faq-question');
 const faqcontent = document.querySelectorAll('.faq-content');
 lastOpened = null;
 lastopenedtoggle = null;
-const product= document.querySelector('.dropdown-elem');
+
+const product= document.querySelectorAll('.dropdown-elem')[0];
+
+const template = document.querySelectorAll('.dropdown-elem')[1];
 
 const flip  = document.querySelector('.flip');
 
-dropdownBtn.forEach(btn => btn.addEventListener('click', function() {
+dropdownBtn.forEach(btn => btn.addEventListener('click', function(e) {
   const menuContent = this.nextElementSibling;
   if (!menuContent.classList.contains("show")) {
-    flip.classList.add('icon-flipped');
+    if(e.target === flip){
+      e.target.classList.add('icon-flipped');
+    }
+    else{
+      e.target.firstElementChild.classList.add('icon-flipped');
+    }
   menuContent.classList.add("show");
   menuContent.classList.remove("hide");
   } else {
   menuContent.classList.add("hide");
   menuContent.classList.remove("show");
-  flip.classList.remove('icon-flipped');
+  if(e.target === flip){
+    e.target.classList.remove('icon-flipped');
+  }
+  else{
+    e.target.firstElementChild.classList.remove('icon-flipped');
+  }
   }
   
   if (lastOpened && lastOpened !== menuContent)
       lastOpened.classList.remove("show");
+      
       lastOpened = menuContent;
 }));
 
 const swingsvg = document.querySelector('.swingsvg');
 
-//write a script that will make the svg blink in time interval
 setInterval(function() {
   swingsvg.classList.toggle('blink');
 }, 100);
 
+//faq component toggle
 faqquestion.forEach(btn=>btn.addEventListener('click', function() {
   console.log(btn);
   const faqcontent = this.nextElementSibling;
@@ -39,7 +53,6 @@ faqquestion.forEach(btn=>btn.addEventListener('click', function() {
   if (!faqcontent.classList.contains("show")) {
     faqcontent.classList.add("show");
   faqcontent.style.display="none";
-  // faqcontent.classList.remove("hide");
   } else {
   faqcontent.style.display="block";
   faqcontent.classList.remove("show");
@@ -49,3 +62,18 @@ faqquestion.forEach(btn=>btn.addEventListener('click', function() {
       lastopenedtoggle.classList.remove("show");
       lastopenedtoggle = faqcontent;
 }));
+
+//hamburger menu
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.menu-items')
+rightnav = document.querySelector('.signup')
+menu = document.querySelector('.menu')
+
+burger.addEventListener('click',()=>{
+    navbar.classList.toggle('h-nav-resp');
+    navlist.classList.toggle('v-class-resp');
+    rightnav.classList.toggle('v-class-resp');
+    menu.classList.toggle('v-class-resp');
+
+})
